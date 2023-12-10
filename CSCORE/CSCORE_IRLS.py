63720a2c0ebb8ff66040deb98c05e17a15271c65
Chang Su <chang.su@emory.edu>
2023-12-10 17:23:51 -0500

    take AnnData object as input

--- CSCORE/CSCORE_IRLS.py
+++ CSCORE/CSCORE_IRLS.py
@@ -6,15 +6,51 @@ Iteratively reweighted least squares (IRLS) procedure in CS-CORE
 
 import numpy as np
 import scipy.stats as stats
-
+from scipy.sparse import issparse
 
 def CSCORE_IRLS(X, seq_depth, post_process=True):
+    """
+    This function implements the iteratively reweighted least squares algorithm of CS-CORE.
+
+    Parameters
+    ----------
+    X: scipy.sparse._csr.csr_matrix or numpy.ndarray
+        Raw UMI count matrix. n cells by p genes.
+    seq_depth: 1-dimensional Numpy array, length n
+        Sum of UMI counts across all genes for each cell.
+    post_process: logical
+        Whether to post-process co-expression estimates to be within [-1,1].
+    max_j: integer
+        Maximum number of iterations in IRLS.
+
+    Returns
+    -------
+    est: p by p Numpy array
+        Estimates of co-expression networks among p genes. Each entry saves the correlation between two genes.
+    p_value: p by p Numpy array
+        p values against H_0: two gene expressions are independent. Please refer to the paper for more details.
+    test_stat:
+        Test statistics against H_0: two gene expressions are independent. Please refer to the paper for more details.
+
+    References
+    ----------
+    Su, C., Xu, Z., Shan, X. et al. Cell-type-specific co-expression inference from single cell RNA-sequencing data.
+    Nat Commun 14, 4846 (2023). https://doi.org/10.1038/s41467-023-40503-7
+    """
     n_cell = X.shape[0]
     n_gene = X.shape[1]
     seq_depth_sq = np.power(seq_depth, 2)
     seq_2 = np.sum(seq_depth_sq)
     seq_4 = np.sum(np.power(seq_depth_sq, 2))
-    mu = np.dot(seq_depth, X) / seq_2
+    if issparse(X):
+        mu = X.transpose().dot(seq_depth) / seq_2
+        # The rest of the computation will use the centered X, which is no longer sparse
+        X = X.toarray()
+    elif isinstance(X, np.ndarray):
+        mu = np.dot(seq_depth, X) / seq_2
+    else:
+        raise ValueError("Unsupported type for X: \n\
+        Matrix X is neither a scipy csr_matrix nor a numpy ndarray. Please reformat the input X.")
     M = np.outer(seq_depth, mu)
     X_centered = X - M
     sigma2 = np.dot(seq_depth_sq, (np.power(X_centered, 2) - M)) / seq_4
@@ -100,3 +136,50 @@ def post_process_est(est):
     est[est > 1] = 1
     est[est < -1] = -1
     return est
+
+
+def CSCORE(adata, gene_index, seq_depth = None):
+    """
+    This function implements CS-CORE for inferring cell-type-specific co-expression networks with scanpy object
+
+    Parameters
+    ----------
+    adata: AnnData
+        Single cell data object. The raw UMI count data are stored as adata.raw.X, which is required for
+    gene_index: 1-dimensional Numpy array, length p
+        Integer indexes for the genes of interest, whose co-expression networks will be estimated in this function.
+
+    Returns
+    -------
+    est: p by p Numpy array
+        Estimates of co-expression networks among p genes. Each entry saves the correlation between two genes.
+    p_value: p by p Numpy array
+        p values against H_0: two gene expressions are independent. Please refer to the paper for more details.
+    test_stat:
+        Test statistics against H_0: two gene expressions are independent. Please refer to the paper for more details.
+
+    References
+    ----------
+    Su, C., Xu, Z., Shan, X. et al. Cell-type-specific co-expression inference from single cell RNA-sequencing data.
+    Nat Commun 14, 4846 (2023). https://doi.org/10.1038/s41467-023-40503-7
+    """
+    # check the size of gene set
+    if len(gene_index) > 10000:
+        print('We suggest running CS-CORE for only the highly expressed genes in cell types.\n\
+        Lowly expressed genes are too sparse for co-expression estimation.')
+    # evaluate sequencing depths
+    if seq_depth is None:
+        seq_depth = adata.raw.X.sum(axis = 1).A1
+    else:
+        if len(seq_depth) != adata.raw.X.shape[0]:
+            raise ValueError('The length of sequencing depths does not match the dimension of the count matrix.')
+    # extract raw UMI count data for running CS-CORE
+    if adata.raw is None:
+        raise TypeError('Raw UMI counts cannot be found in adata.raw.X.')
+    elif not hasattr(adata.raw, "X"):
+        raise TypeError('Raw UMI counts cannot be found in adata.raw.X.')
+
+    res = CSCORE_IRLS(adata.raw.X[:, gene_index],
+                      seq_depth,
+                      post_process=True)
+    return res
--- setup.py
+++ setup.py
@@ -13,5 +13,6 @@ setup(
     install_requires=[
         'numpy',
         "scipy"
+        "scanpy"
     ]
 )
