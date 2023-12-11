303fb1aefa3da023484621c12f5a413d2e90cd43
Chang Su <chang.su@emory.edu>
2023-12-10 21:09:06 -0500

    update version and license

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ from setuptools import setup
 
 setup(
     name='CSCORE',
-    version='0.0.1',
+    version='1.0.0',
     author='Chang Su',
     author_email='chang.su@emory.edu',
     url='https://github.com/ChangSuBiostats/CS-CORE_python',
-    license='LICENSE',
+    license='MIT',
     packages=['CSCORE'],
     description='A Python package for cell-type-specific co-expression inference from single cell RNA-sequencing data',
     python_requires=">3",
     install_requires=[
         'numpy',
         "scipy",
-        "scanpy"
+        'scanpy'
     ]
 )
